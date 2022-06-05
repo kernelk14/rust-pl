818a975f8523d74bd0369c7a525c1aff95ca1fd7
kernelk14 <khyleisaias@gmail.com>
2022-06-05 09:38:56 +0800

    RPL: Added files

---
+++ main.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+struct Lexer<'a> {
+    program: &'a str,
+}
+struct Parser<'a> {
+    prog: &'a str,
+}
+
+impl Lexer<'_> {
+    pub fn tokenize() {todo!()}
+    pub fn evaluate() {todo!()}
+}
+impl Parser<'_> {
+    pub fn codegen() {todo!()}
+    pub fn parse() {todo!()}
+}
+
+fn main() {
+    // return Parser;
+    // return Lexer;
+    todo!();
+}
